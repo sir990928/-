3747cf04251209a1157370975ca491adc3dfc60e
sir990928 <sir990928@xxx.com>
2026-08-09 20:20:43 +0800

    @

---
+++ SukiSU-Ultra/manager/app/src/main/java/com/sukisu/ultra/ui/viewmodel/MainActivityUiState.kt
@@ -0,0 +1,16 @@
+package com.sukisu.ultra.ui.viewmodel
+
+import androidx.compose.runtime.Immutable
+import com.sukisu.ultra.ui.UiMode
+import com.sukisu.ultra.ui.theme.AppSettings
+
+@Immutable
+data class MainActivityUiState(
+    val appSettings: AppSettings,
+    val pageScale: Float,
+    val enableBlur: Boolean,
+    val enableFloatingBottomBar: Boolean,
+    val enableFloatingBottomBarBlur: Boolean,
+    val enableNavigationBadge: Boolean,
+    val uiMode: UiMode,
+)
